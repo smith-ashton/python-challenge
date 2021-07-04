02ed270d8010489f48ca8a9515fbf56ec0597780
smith-ashton <smith.ashton17@gmail.com>
2021-07-04 11:13:57 -0400

    get number of votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,6 +14,10 @@ with open(csvpath) as csvfile:
     candidate_row = []
     count = 0
     cand_list = []
+    vote_count = 0
+    cand_count = 0
+    cand_count2 = 0
+
     for row in csvreader:
         candidate_row.append(row[2])
         count = count + 1
@@ -21,11 +25,14 @@ with open(csvpath) as csvfile:
     for x in candidate_row:
         if x not in cand_list:
             cand_list.append(x)
-    print(cand_list)   
+            cand_count = cand_count + 1
 
-
-            
-
+    while cand_count2 < cand_count:
+        for x in candidate_row:
+            if x == cand_list[cand_count2]:
+                vote_count = vote_count + 1
+        print (cand_list[cand_count2], vote_count)
+        cand_count2 = cand_count2 + 1
 
 
 #The total number of votes cast
