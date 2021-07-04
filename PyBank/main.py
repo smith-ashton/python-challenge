6dba92d312783f43218e022bf0403162c1465939
smith-ashton <smith.ashton17@gmail.com>
2021-07-04 09:04:06 -0400

    calculate total and average

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,12 +15,21 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-#Calculate total number on months included in the dataset
     count = 0
+    total = 0
     for row in csvreader:
+#Calculate total number on months included in the dataset
         count = count + 1
-    print (count)
 #Calculate net total amount of "Profit/Losses" over the entire period
+        total = total + float(row[1])
+
+    average = total / count
+    print (count)
+    print(total)
+    print (average)
+    
+
+#Calculate average of the changes in "Profit/Losses" over the entire period
 #Calculate average of the changes in "Profit/Losses" over the entire period
 #Calculate greatest increase in profits (date and amount) over the entire period
 #Calculate greatest decrease in losses (date and amount) over the entire period
